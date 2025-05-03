f54a7c235ff5f37bcdeff8ed54456ca5124082c6
iamzuchi <hovadavinci@gmail.com>
2025-05-03 18:05:45 +0100

    Begin project

---
+++ src/utils/jwt.ts
@@ -0,0 +1,32 @@
+import jwt, { SignOptions } from "jsonwebtoken";
+import { UserDocument } from "../models/user.model"
+import { config } from "../config/app.config";
+
+export type AccessTPayload = {
+    userId: UserDocument["_id"];
+};
+
+type SignOptsAndSecret = SignOptions & {
+    secret: string;
+};
+
+const defaults: SignOptions = {
+    audience: ["user"],
+};
+
+export const accessTokenSignOptions: SignOptsAndSecret = {
+    expiresIn: config.JWT_EXPIRES_IN as any,
+    secret: config.JWT_SECRET,
+};
+
+export const signJWToken = (
+    payload: AccessTPayload,
+    options?: SignOptsAndSecret
+) =>  {
+    const { secret, ...opts } = options || accessTokenSignOptions;
+    return jwt.sign(payload, secret, {
+        ...defaults,
+        ...opts,
+    });
+};
+
